a6d97193502df698f78b40b7551238d46652f34d
Willishakespeare <Skr13@outlook.com>
2020-10-08 13:43:31 -0700

    add Theme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import type { AppProps } from 'next/app';
+import { ThemeProvider } from 'emotion-theming';
 import GlobalStyles from '@Styles/globalStyled';
+import theme from '@Styles/theme';
 
 const App: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <GlobalStyles />
       <Component {...pageProps} />
-    </>
+    </ThemeProvider>
   );
 };
 export default App;
---
+++ src/styles/themes/themeColors.ts
@@ -0,0 +1,50 @@
+const ThemeColor = {
+  primary: {
+    base: '#2E384C',
+    light: '#414E6A',
+    dark: '#58698C'
+  },
+  accent: {
+    green: {
+      base: '#269E40',
+      light: '#35CE57',
+      dark: '#95E1A6'
+    },
+    red: {
+      base: '#D41A47',
+      light: '#E63B64',
+      dark: '#E3718D'
+    },
+    orange: {
+      base: '#EA7520',
+      light: '#FF9B53',
+      dark: '#F4B384'
+    },
+    blue: {
+      base: '#1C61C9',
+      light: '#3E82E9',
+      dark: '#82A9E5'
+    },
+    cyan: {
+      base: '#2BB2A1',
+      light: '#46DBC9',
+      dark: '#9AEFE5'
+    },
+    pink: {
+      base: '#C82874',
+      light: '#DB468D',
+      dark: '#EE8ABA'
+    }
+  },
+  gray: {
+    500: '#495161',
+    400: '#90959E',
+    300: '#BCBCBC',
+    200: '#DBDCDF',
+    100: '#FFFFFF'
+  },
+  black: '#303030',
+  white: '#FFFFFF'
+};
+
+export default ThemeColor;
