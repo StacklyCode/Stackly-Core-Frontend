e13ff8f6df189a3afa013719fb8bcbfcdeb2937f
erianvc <erianvc.dev@gmail.com>
2020-10-10 14:08:19 -0500

    feat: add atom title component

--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
 import styled from '@Styles/styled';
 import Button from '@Atoms/button';
+import Title from '@Atoms/title';
 
 type TextProps = {
   color: string;
@@ -14,14 +14,36 @@ const Text = styled.div<TextProps>`
   })}
 `;
 
+const Background = styled.main`
+  background: ${({ theme }) => theme.colors.primary.dark};
+  min-height: 100vh;
+  width: 100%;
+`;
+
 const TestPage: React.FC = () => {
   return (
-    <>
+    <Background>
+      <Title bold>We create the right solution for your business.</Title>
+      <Title size="TitleMedium" color="gray">
+        We create the right solution for your business.
+      </Title>
+      <Title size="TitleSmall" color="black" bold>
+        We create the right solution for your business.
+      </Title>
+      <Title size="SubTitleLarge" color="white">
+        We create the right solution for your business.
+      </Title>
+      <Title size="SubTitleMedium" color="gray" bold>
+        We create the right solution for your business.
+      </Title>
+      <Title size="SubTitleSmall" color="black">
+        We create the right solution for your business.
+      </Title>
       <Button>Let’s start a project</Button>
       <Button color="primary">Let’s start a project</Button>
       <Button outline>Let’s start a project</Button>
       <Text color="blue">StyledCode</Text>
-    </>
+    </Background>
   );
 };
 
