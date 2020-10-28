0df1b593acc094a45a9a7ebe002b4880ca5e27a8
Willishakespeare <Skr13@outlook.com>
2020-10-27 19:20:17 -0700

    fix title colors

--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -1,6 +1,8 @@
 import styled from '@Styles/styled';
 import Button from '@Atoms/button';
 import Title from '@Atoms/title';
+import Input from '@Atoms/input';
+import Icon from '@Atoms/icon';
 
 type TextProps = {
   color: string;
@@ -15,7 +17,7 @@ const Text = styled.div<TextProps>`
 `;
 
 const Background = styled.main`
-  background: ${({ theme }) => theme.colors.primary.dark};
+  background: ${({ theme }) => theme.colors.primary.light};
   min-height: 100vh;
   width: 100%;
 `;
@@ -43,6 +45,20 @@ const TestPage: React.FC = () => {
       <Button color="primary">Let’s start a project</Button>
       <Button outline>Let’s start a project</Button>
       <Text color="blue">StyledCode</Text>
+
+      <Icon icon="facebook" size="100px" color="dark" />
+      <Icon icon="facebook" size="50px" color="light" />
+      <Icon icon="facebook" size="20px" color="grey" />
+
+      <Input id="checkbox" type="checkbox" color="dark" />
+      <Input id="password" type="password" color="dark" placeholder="Password" icon="lock" />
+      <Input type="email" icon="facebook" />
+      <Input type="text" />
+      <Input type="search" />
+      <Input type="number" />
+      <Input />
+      <Input />
+      <Input id="customInput" />
     </Background>
   );
 };
