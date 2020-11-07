7d361d6ec709c9436afecbc4466b87732827aac8
Willishakespeare <Skr13@outlook.com>
2020-11-07 09:22:39 -0700

    Version 1.01

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -12,10 +12,6 @@ const App: React.FC<AppProps> = ({ Component, pageProps }) => {
       <Head>
         <title>Stackly Code</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@500;700&family=Roboto:wght@500;700&display=swap"
-          rel="stylesheet"
-        />
       </Head>
       <Component {...pageProps} />
     </ThemeProvider>
