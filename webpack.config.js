ce078dcffab02360420a486456b7fc77d259e56c
Jean Pierre Ortiz Murcia <ortizmurciajeanpierre@gmail.com>
2020-11-29 12:44:26 -0500

    feat: add mode in webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,14 @@
-const LoadablePlugin = require("@loadable/webpack-plugin");
+const LoadablePlugin = require('@loadable/webpack-plugin')
+
+const mode =
+  process.env.NODE_ENV === 'production' ? 'production' : 'development'
+
 module.exports = {
-  // ...
   plugins: [new LoadablePlugin()],
-};
+  mode,
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+    },
+  },
+}
