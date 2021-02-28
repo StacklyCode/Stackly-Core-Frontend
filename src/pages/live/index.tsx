1a33a66d5d93f9bbc9a7b4933ba527d2da64c238
Fernando Lopez <lopezfer@globalhitss.com>
2021-02-28 12:21:01 -0300

    fix: player

--- src/pages/live/index.tsx
+++ src/pages/live/index.tsx
@@ -1,10 +1,10 @@
-import React, { ReactElement } from "react";
-import TemplateMain from "@Src/components/templates";
-import AtomContainer from "@Src/components/atoms/container";
-import I18n from "@Src/i18n";
-import { TFunction } from "next-i18next";
-import { GetStaticProps } from "next";
-import ReactPlayer from 'react-player'
+import React, { ReactElement } from 'react';
+import TemplateMain from '@Src/components/templates';
+import AtomContainer from '@Src/components/atoms/container';
+import I18n from '@Src/i18n';
+import { TFunction } from 'next-i18next';
+import { GetStaticProps } from 'next';
+import ReactPlayer from 'react-player';
 interface Props {
   t?: TFunction;
 }
@@ -13,12 +13,17 @@ const PageLive = ({ t }: Props) => {
   return (
     <TemplateMain t={t}>
       <AtomContainer>
-        <ReactPlayer url={"http://amssamples.streaming.mediaservices.windows.net/69fbaeba-8e92-4740-aedc-ce09ae945073/AzurePromo.ism/manifest(format=mpd-time-csf)"} config={
-          {
+        <ReactPlayer
+          controls={true}
+          url={
+            'http://amssamples.streaming.mediaservices.windows.net/69fbaeba-8e92-4740-aedc-ce09ae945073/AzurePromo.ism/manifest(format=mpd-time-csf)'
+          }
+          config={{
             file: {
-              forceDASH: true
-            }
-          }} />
+              forceDASH: true,
+            },
+          }}
+        />
       </AtomContainer>
     </TemplateMain>
   );
@@ -27,8 +32,8 @@ const PageLive = ({ t }: Props) => {
 export const getStaticProps: GetStaticProps = async () => {
   return {
     props: {
-      namespacesRequired: ["common"],
+      namespacesRequired: ['common'],
     },
   };
 };
-export default I18n.withTranslation(["common"])(PageLive);
+export default I18n.withTranslation(['common'])(PageLive);
