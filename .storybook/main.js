2be5fa6f84d58f79489515dbe578d3a2b423893f
Willishakespeare <skr13@outlook.com>
2021-04-12 21:53:01 -0700

    feat: fix webpack2

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,13 +1,10 @@
-const { TsconfigPathsPlugin } = require('tsconfig-paths-webpack-plugin');
-
 module.exports = {
-  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
-  webpackFinal: async (config) => {
-    [].push.apply(config.resolve.plugins, [
-      new TsconfigPathsPlugin({ extensions: config.resolve.extensions }),
-    ]);
-
-    return config;
-  },
-};
+  "stories": [
+    "../src/**/*.stories.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials"
+  ]
+}
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,25 +1,10 @@
-import React from 'react';
-import { addDecorator } from '@storybook/react';
-import { Dark, Light } from '../src/styles/theme';
-import { withThemesProvider } from 'storybook-addon-emotion-theme';
-import GlobalStyles from '../src/styles/globalStyled';
 
 export const parameters = {
-  actions: { argTypesRegex: '^on[A-Z].*' },
+  actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
     },
   },
-};
-
-addDecorator((story) => (
-  <>
-    <GlobalStyles />
-    <div style={{ padding: '3rem' }}>{story()}</div>
-  </>
-));
-
-const themes = [Light, Dark];
-addDecorator(withThemesProvider(themes));
+}
