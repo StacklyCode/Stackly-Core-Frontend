ad7ef9c663ab4b253f550bf5354b744685071f7f
alexTexis <alextexis.js@gmail.com>
2021-06-07 10:38:30 -0500

    feat(workshops page): workshops grid

--- src/pages/workshops/index.tsx
+++ src/pages/workshops/index.tsx
@@ -2,11 +2,13 @@ import { FC } from 'react'
 import { GetStaticProps } from 'next'
 import TemplateMain from '@Templates/index'
 import Search from '@Src/components/sections/workshops/search'
+import WorkshopsGrid from '@Src/components/sections/workshops/workshopsgrid'
 
 const PageTeam: FC = () => {
   return (
     <TemplateMain SeoTitle="Stackly Team" SeoDesc="Workshops" SeoPage="">
       <Search />
+      <WorkshopsGrid />
     </TemplateMain>
   )
 }
